7f8853156dfbb434dca7b772acc403ce86c8e946
HobbitAvenger <superhobbitavenger@gmail.com>
2022-02-08 13:36:11 -0600

    Basic File Structure/ Player Controls

---
+++ JADS/Assets/_Scripts/Player/MovementScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    
+    private Vector3 velocityVector;
+    private Rigidbody2D rigidBody2D;
+
+    [SerializeField] private float moveSpeed;
+
+
+    private void Awake(){
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void SetVelocity(Vector3 velocityVector){
+        this.velocityVector = velocityVector;
+    }
+
+    private void FixedUpdate(){
+        rigidBody2D.velocity = velocityVector * moveSpeed;
+    }
+
+
+
+}
