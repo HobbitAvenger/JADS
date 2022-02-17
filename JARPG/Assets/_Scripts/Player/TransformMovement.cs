70482fb3e988d4ec3127dc6be27e75f5c05e874e
HobbitAvenger <superhobbitavenger@gmail.com>
2022-02-16 23:35:48 -0600

    Updated to most recent LTS version

--- JARPG/Assets/_Scripts/Player/TransformMovement.cs
+++ JARPG/Assets/_Scripts/Player/TransformMovement.cs
@@ -2,27 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementScript : MonoBehaviour
+public class TransformMovement : MonoBehaviour, IMovement
 {
-    
     private Vector3 velocityVector;
     private Rigidbody2D rigidBody2D;
 
     [SerializeField] private float moveSpeed;
 
 
-    private void Awake(){
-        rigidBody2D = GetComponent<Rigidbody2D>();
-    }
 
     public void SetVelocity(Vector3 velocityVector){
         this.velocityVector = velocityVector;
     }
 
-    private void FixedUpdate(){
-        rigidBody2D.velocity = velocityVector * moveSpeed;
+    private void Update(){
+        transform.position += velocityVector * moveSpeed * Time.deltaTime;
     }
-
-
-
 }
