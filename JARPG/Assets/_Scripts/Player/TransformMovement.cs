05551e0d1e55e53b28e48f4374988b418b300288
HobbitAvenger <superhobbitavenger@gmail.com>
2022-02-23 22:26:19 -0600

    Revert "Changed to Godot"
    This reverts commit 63100c7c1cfa9c4dc7722aaa6971c8280fece2c9.

---
+++ JARPG/Assets/_Scripts/Player/TransformMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformMovement : MonoBehaviour, IMovement
+{
+    private Vector3 velocityVector;
+    private Rigidbody2D rigidBody2D;
+
+    [SerializeField] private float moveSpeed;
+
+
+
+    public void SetVelocity(Vector3 velocityVector){
+        this.velocityVector = velocityVector;
+    }
+
+    private void Update(){
+        transform.position += velocityVector * moveSpeed * Time.deltaTime;
+    }
+}
