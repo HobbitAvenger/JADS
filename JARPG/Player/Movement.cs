dc7045f10463e195135bcacc2977c81d513f94b2
AbeamusBrandon <ldphilbin@gmail.com>
2022-03-06 08:48:12 -0600

    Minor changes
    Refactored code (for my OCD)
    Added aspace to sprint keys (because shift is already interact and it's easier to press space than shift repeatedly)

--- JARPG/Player/Movement.cs
+++ JARPG/Player/Movement.cs
@@ -5,51 +5,52 @@ using System;
 public class Movement:KinematicBody2D
 
 {
-	public int maxspeed = 500;
-	public int maxsprint =750;
+	public int maxSpeed = 500;
+	public int maxSprint =750;
 	public int acceleration = 10000;
 	public int friction  = 10000;
 	
-	Vector2 playervelocity;
+	Vector2 playerVelocity;
 	
 	public override void _Ready()
 	{
 		GD.Print("Hello World!");
-		playervelocity.x= 0;
-		playervelocity.y= 0;
+		playerVelocity.x = 0;
+		playerVelocity.y = 0;
 		//var animationPlayer = $AnimationPlayer;
 	}
 	
 	public override void _Process(float delta)
 	{
-		var input_vector = Vector2.Zero;
+		var inputVector = Vector2.Zero;
 		
-		input_vector.x = Input.GetActionStrength("right")-Input.GetActionStrength("left");
-		input_vector.y = Input.GetActionStrength("up")-Input.GetActionStrength("down");
-		input_vector = input_vector.Normalized();
+		inputVector.x = Input.GetActionStrength("right") - Input.GetActionStrength("left");
+		inputVector.y = Input.GetActionStrength("up") - Input.GetActionStrength("down");
+		inputVector = inputVector.Normalized();
 		
-		if (input_vector != Vector2.Zero)
+		if (inputVector != Vector2.Zero)
 		{
-			if(Input.GetActionStrength("sprint") == 1)
+			if (Input.GetActionStrength("sprint") == 1)
 			{
-				playervelocity += input_vector * acceleration*delta;
-				playervelocity = playervelocity.Clamped(length: maxsprint);
+				playerVelocity += inputVector * acceleration * delta;
+				playerVelocity = playerVelocity.Clamped(length: maxSprint);
 			}
 			else
 			{
-				playervelocity += input_vector * acceleration*delta;
-				playervelocity = playervelocity.Clamped(length: maxspeed);
+				playerVelocity += inputVector * acceleration * delta;
+				playerVelocity = playerVelocity.Clamped(length: maxSpeed);
 			}
 			
 		}
+
 		else
 		{
-			playervelocity = playervelocity.MoveToward(Vector2.Zero, friction * delta);
+			playerVelocity = playerVelocity.MoveToward(Vector2.Zero, friction * delta);
 		}
 		
-		GD.Print(playervelocity);
+		GD.Print(playerVelocity);
 		
 		
-		playervelocity = MoveAndSlide(playervelocity);
+		playerVelocity = MoveAndSlide(playerVelocity);
 	}
 }
